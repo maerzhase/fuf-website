ab6f08d787739329b769664c5e064a0c787afbc9
maerzhase <markus@lili16.de>
2017-02-20 23:05:18 +0100

    updates next prev fn to overlay

--- src/components/OverlaySection/index.js
+++ src/components/OverlaySection/index.js
@@ -29,9 +29,34 @@ export default class OverlaySection extends React.Component {
 
   @autobind
   _toggleGallery(e){
-    this.setState({
-      galleryOpen: !this.state.galleryOpen,
-    })
+    const newVal = !this.state.galleryOpen;
+    if(this.props.onToggleGallery) this.props.onToggleGallery(newVal);
+  }
+
+  @autobind
+  _next(){
+    const params = this.context.router.params;
+    const currentId = Number(params.id);
+    const maxLength = this.state.images.length;
+    let newVal = 1;
+    if(currentId == maxLength) newVal = 1;
+    if(currentId < maxLength) newVal = currentId+1;  
+    const path = this.context.router.location.pathname;
+    const newpath = `${path.substring(0,path.length-1)}${newVal}`;
+    this.context.router.push(newpath);
+  }
+  
+  @autobind
+  _prev(){
+    const params = this.context.router.params;
+    const currentId = Number(params.id);
+    const maxLength = this.state.images.length;
+    let newVal = 1;
+    if(currentId == 0) newVal = maxLength;
+    if(currentId > 1) newVal = currentId-1;  
+    const path = this.context.router.location.pathname;
+    const newpath = `${path.substring(0,path.length-1)}${newVal}`;
+    this.context.router.push(newpath);
   }
 
   render(){
@@ -41,6 +66,10 @@ export default class OverlaySection extends React.Component {
     const galleryClass = classnames(styles.gallery,{isOpen:galleryOpen});
     const arrowClass   = classnames(styles.arrow,{moveLeft:galleryOpen});
     const arrowIcon = galleryOpen ? <i className="material-icons">arrow_drop_down</i> : <i className="material-icons">arrow_drop_down</i>;
+    const params = this.context.router.params;
+    const currentId = Number(params.id);
+    const inlineStyle = {transform:`translate(-${(currentId-1)}00%,0)`}
+    console.log(inlineStyle)
     return(
       <div className={componentClass}>
         <div className={styles.scrollContainer}>
@@ -55,10 +84,13 @@ export default class OverlaySection extends React.Component {
         </div>
 
         <div className={galleryClass} >
+          <div className="container" style={inlineStyle}>
           {images.map((image,i)=>{
             const style = {backgroundImage:`url(${image.retina})`};
-            return <div key={i} style={style}/>
+            return <div className={styles.img} key={i} style={style}/>
           })}
+          </div>
+          <div className={styles.next} onMouseDown={this._next}><i className="material-icons">skip_next</i></div>
           <label>{credits}</label>
         </div>
       </div>
@@ -71,4 +103,8 @@ OverlaySection.defaultProps = {
   galleryOpen:false,
   images: [],
   credits:"",
+}
+
+OverlaySection.contextTypes = {
+  router: React.PropTypes.object,
 }

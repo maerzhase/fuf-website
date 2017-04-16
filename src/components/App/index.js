b8425fccfd2301923517e99bab2ad877c79839ba
maerzhase <markus@lili16.de>
2017-04-16 19:38:32 +0200

    first try implementing controlled scrolling

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,15 @@ import AppActions from '../../flux/actions/AppActions';
 
 const JSONFile = require('../../assets/data.json')
 
+const routes = [
+  '/start',
+  '/lust',
+  '/fiktion',
+  '/frauen',
+  '/termine',
+  '/kontakt'
+]
+
 export default class App extends Component{
 
   constructor(props){
@@ -24,6 +33,42 @@ export default class App extends Component{
     initialActions.forEach((a)=>{
       a();
     })
+    this._interval = setInterval(this.handleScroll,1);
+    this.lastOffset = 0;
+    this.routeIndex = 0;
+  }
+
+  resetInterval = (time) => {
+    clearInterval(this._interval);
+    setTimeout(()=>{
+      this._interval = setInterval(this.handleScroll,1);
+      this.lastAction = '';
+      this.lastOffset = window.pageYOffset;
+    },time);
+  }
+
+  handleScroll = () => {
+     const offset = window.pageYOffset;
+     if(offset === this.lastOffset) return;
+     if (offset > this.lastOffset && this.lastAction != 'down') {
+      this.lastAction = 'down';
+      this.lastOffset = offset;
+      this.routeIndex = this.routeIndex + 1;
+      if(this.routeIndex >= routes.length) this.routeIndex = 0;
+      this.context.router.push(routes[this.routeIndex])
+      this.resetInterval(1900);
+      console.log('down', this.routeIndex)
+
+     } else if (offset < this.lastOffset && this.lastAction != 'up') {
+      this.lastAction = 'up';
+      this.lastOffset = offset;
+      this.routeIndex = this.routeIndex - 1;
+      if(this.routeIndex <= 0) this.routeIndex = 0;
+      this.context.router.push(routes[this.routeIndex])
+      this.resetInterval(1900);
+      console.log('up', this.routeIndex);
+     }
+
   }
 
   render(){
--- src/lib/ScrollPilot.js
+++ src/lib/ScrollPilot.js
@@ -1,3 +1,40 @@
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
+
+
 const scrollToElement = (ref, duration) => {
   const target = ref.getBoundingClientRect().top;
 
@@ -16,9 +53,11 @@ const scrollToElement = (ref, duration) => {
       window.requestAnimationFrame(step);
     }else{
       if(iToggled)document.body.style.overflowY = "initial"
+      enableScroll();
     }
   }
-  window.requestAnimationFrame(step);       
+  window.requestAnimationFrame(step); 
+  disableScroll();      
 }
 
 const scrollEasing = (current_time, beginning_value, change_value, total_time) => {

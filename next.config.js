eb66cf796184e577e88d2a3b1b396c56b7cedbae
maerzhase <markus@lili16.de>
2019-10-14 08:21:32 +0100

    add custom exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,8 +1,60 @@
 const dotenv = require('dotenv');
 const webpack = require('webpack');
-const clearRequireCachePlugin = require('webpack-clear-require-cache-plugin');
+const projects = [
+  {
+    link: 'care',
+    gallery: [
+      ['image', '/static/care/01.jpg'],
+      ['image', '/static/care/02.jpg'],
+      ['image', '/static/care/03.jpg'],
+      ['image', '/static/care/04.jpg'],
+    ],
+  },
+  {
+    link: 'glamour',
+    gallery: [
+      ['image', '/static/glamour/01.jpg'],
+      ['image', '/static/glamour/02.jpg'],
+      ['image', '/static/glamour/03.jpg'],
+      ['image', '/static/glamour/04.jpg'],
+    ],
+  },
+  {
+    link: 'weapon',
+    gallery: [
+      ['embed', 'https://player.vimeo.com/video/309847175'],
+      ['image', '/static/weapon/01.jpg'],
+      ['image', '/static/weapon/02.jpg'],
+      ['image', '/static/weapon/03.jpg'],
+      ['image', '/static/weapon/04.jpg'],
+      ['image', '/static/weapon/05.jpg'],
+      ['image', '/static/weapon/06.jpg'],
+      ['image', '/static/weapon/07.jpg'],
+      ['image', '/static/weapon/08.jpg'],
+    ],
+  },
+  {
+    link: 'lust',
+    gallery: [
+      ['image', '/static/lust/01.jpg', '© Paula Reissig'],
+      ['embed', 'https://player.vimeo.com/video/188169589'],
+      ['image', '/static/lust/02.jpg', '© Paula Reissig'],
+      ['image', '/static/lust/03.jpg', '© Paula Reissig'],
+      ['image', '/static/lust/04.jpg', '© Paula Reissig'],
+      ['image', '/static/lust/05.jpg', '© Paula Reissig'],
+      ['image', '/static/lust/06.jpg', '© Paula Reissig'],
+      ['image', '/static/lust/07.jpg', '© Paula Reissig'],
+    ],
+  },
+  {
+    link: 'fiktion',
+    gallery: [
+      ['image', '/static/fiktion/01.jpg', '© Sebastian Pircher'],
+      ['image', '/static/fiktion/02.jpg', '© Sebastian Pircher'],
+    ],
+  },
+];
 
-const isDev = process.env.NODE_ENV !== 'production';
 
 exports.webpack = config =>
   Object.assign(config, {
@@ -19,3 +71,22 @@ exports.webpack = config =>
       //   ]),
     ],
   });
+
+const pages = {
+  '/': { page: '/' },
+  ...projects.reduce((acc,p) => {
+    acc[`/${p.link}/index`] = { page: '/[projectId]/index'};
+    p.gallery.forEach((image, index) => {
+      acc[`/${p.link}/${index}`] = { page: '/[projectId]/[imageId]'};
+    });
+    return acc;
+  }, {}),
+};
+console.log(pages);
+
+exports.exportPathMap = async function(
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return pages
+  };

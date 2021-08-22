d2b8cb17fac476ada6de50070fa16f5c95a8c950
maerzhase <markus@lili16.de>
2021-08-22 18:56:41 +0200

    fix footer alignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import Container from "./container";
+import Container from "./Container";
 import { EXAMPLE_PATH } from "@/api/constants";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
@@ -9,21 +9,23 @@ const LINKS = ["instagram", "facebook", "datenschutz", "impressum"];
 export default function Footer() {
   return (
     <footer>
-      <Box mt={10} display="flex" alignItems="center" width="100%">
-        <Box flexGrow={1} display="flex" alignItems="center">
-          <Typography variant="subtitle1">
-            {" "}
-            &copy; {new Date().getFullYear()} Frauen und Fiktion
-          </Typography>
+      <Container>
+        <Box mt={10} display="flex" alignItems="center" width="100%">
+          <Box flexGrow={1} display="flex" alignItems="center">
+            <Typography variant="subtitle1">
+              {" "}
+              &copy; {new Date().getFullYear()} Frauen und Fiktion
+            </Typography>
+          </Box>
+          <Box>
+            {LINKS.map((l) => (
+              <Button key={l} size="small">
+                {l}
+              </Button>
+            ))}
+          </Box>
         </Box>
-        <Box>
-          {LINKS.map((l) => (
-            <Button key={l} size="small">
-              {l}
-            </Button>
-          ))}
-        </Box>
-      </Box>
+      </Container>
     </footer>
   );
 }

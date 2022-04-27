64e4b43f13e12dc0f3ea60b0bfd4604a758caf9f
maerzhase <markus@lili16.de>
2022-04-27 16:02:28 +0200

    fix typo in about headline

--- src/pages/about.js
+++ src/pages/about.js
@@ -110,7 +110,7 @@ export default function Index({ preview, aboutPage }) {
             ))}
             <Box className={classes.collaboratorWrapper} display="flex" mb={4}>
               <Typography variant="h3" className={classes.collaboratorHeadline}>
-                Kolaborateur*innen
+                Kollaborateu*innen
               </Typography>
               <div className={classes.collaborators}>
                 {aboutPage.collaborators.map((c) => (

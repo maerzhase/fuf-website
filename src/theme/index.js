6fe76bff28d6dc3f45e43e4e81ef78b39eaf2851
maerzhase <markus@lili16.de>
2022-04-27 16:27:28 +0200

    fix commuters sans including bold cut

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,7 @@
 import { createTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 
-const SANS = ["Commuters Sans Regular", "system-ui", "sans-serif"];
+const SANS = ["Commuters Sans", "system-ui", "sans-serif"];
 const SANS_HEADLINE = ["Trash", "system-ui", "sans-serif"];
 
 const WHITE = "#ffffff";

2e85ab08040e20ea4a80357e7255df4660610e31
maerzhase <markus@lili16.de>
2022-09-01 11:39:13 +0200

    move about text more down

--- src/pages/about.js
+++ src/pages/about.js
@@ -117,7 +117,7 @@ export default function Index({ preview, aboutPage }) {
             minHeight="100vh"
             display="flex"
             alignItems="center"
-            mt={8}
+            mt={23}
             mb={8}
           >
             <Typography variant="h5">
